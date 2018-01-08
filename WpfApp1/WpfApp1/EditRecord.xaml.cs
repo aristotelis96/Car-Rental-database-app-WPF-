04f69771c4429227af5fd9ef133354c1578da5ba
aristotelis96 <aristotelis96@users.noreply.github.com>
2018-01-08 17:10:53 +0200

    Edit for store

--- WpfApp1/WpfApp1/EditRecord.xaml.cs
+++ WpfApp1/WpfApp1/EditRecord.xaml.cs
@@ -30,9 +30,14 @@ namespace WpfApp1
                 EmployeePostalCodeTextBox.Text = _drv["postalcode"].ToString();
                 EmployeeCityTextBox.Text = _drv["city"].ToString();
             }
-            else if (_drv.DataView.Table.ToString() == "")
+            else if (_drv.DataView.Table.ToString() == "store")
             {
-                
+                StoreGrid.Visibility = Visibility.Visible;
+                StoreIDTextBox.Text = _drv["storeid"].ToString();
+                StoreStreetTextBox.Text = _drv["street"].ToString();
+                StoreStreetNumberTextBox.Text = _drv["streetnumber"].ToString();
+                StorePostalCodeTextBox.Text = _drv["postalcode"].ToString();
+                StoreCityTextBox.Text = _drv["city"].ToString();
             }
             else if (_drv.DataView.Table.ToString() == "")
             {
@@ -47,18 +52,34 @@ namespace WpfApp1
 
         private void Update_OnClick(object sender, RoutedEventArgs e)
         {
-            App.RunCommand("update employee set firstname='" + EmployeeFirstNameTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
-            App.RunCommand("update employee set lastname='" + EmployeeLastNameTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
-            App.RunCommand("update employee set street='" + EmployeeStreetTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
-            App.RunCommand("update employee set streetnumber='" + EmployeeNumberTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
-            App.RunCommand("update employee set postalcode='" + EmployeePostalCodeTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
-            App.RunCommand("update employee set city='" + EmployeeCityTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
-            App.RunCommand("update employee set socialSecurityNumber='" + EmployeeSocialSecurityNumberTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
-            App.RunCommand("update employee set driverlicense='" + EmployeeDriverLicenceTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
+            if (_drv.DataView.Table.ToString() == "Employee")
+            {
 
-            App.RefreshDataGrid();
-            MessageBox.Show("Employee updated succesfully!", "Success");
-         Close();   
+                App.RunCommand("update employee set firstname='" + EmployeeFirstNameTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
+                App.RunCommand("update employee set lastname='" + EmployeeLastNameTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
+                App.RunCommand("update employee set street='" + EmployeeStreetTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
+                App.RunCommand("update employee set streetnumber='" + EmployeeNumberTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
+                App.RunCommand("update employee set postalcode='" + EmployeePostalCodeTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
+                App.RunCommand("update employee set city='" + EmployeeCityTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
+                App.RunCommand("update employee set socialSecurityNumber='" + EmployeeSocialSecurityNumberTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
+                App.RunCommand("update employee set driverlicense='" + EmployeeDriverLicenceTextBox.Text + "' where employee.irs_number=" + int.Parse(EmployeeIRSNumberTextBox.Text) + ";");
+
+                App.RefreshDataGrid();
+                MessageBox.Show("Employee updated succesfully!", "Success");
+                Close();
+            }
+            else if (_drv.DataView.Table.ToString() == "store")
+            {
+
+                App.RunCommand("update store set street='" + StoreStreetTextBox.Text + "' where store.storeid=" + int.Parse(StoreIDTextBox.Text) + ";");
+                App.RunCommand("update store set streetnumber='" + StoreStreetNumberTextBox.Text + "' where store.storeid=" + int.Parse(StoreIDTextBox.Text) + ";");
+                App.RunCommand("update store set postalcode='" + StorePostalCodeTextBox.Text + "' where store.storeid=" + int.Parse(StoreIDTextBox.Text) + ";");
+                App.RunCommand("update store set city='" + StoreCityTextBox.Text + "' where store.storeid=" + int.Parse(StoreIDTextBox.Text) + ";");
+
+                App.RefreshDataGrid();
+                MessageBox.Show("Store updated succesfully!", "Success");
+                Close();
+            }
         }
     }
 }
